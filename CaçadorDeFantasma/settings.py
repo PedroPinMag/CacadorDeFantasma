0f7a642675c0efb8bb371c358ddc69d1718c5147
Pedro Pinheiro Magalhães <71952546+PedroPinMag@users.noreply.github.com>
2025-05-28 00:17:21 -0300

    Segunda Versão Operacional

--- CaçadorDeFantasma/settings.py
+++ CaçadorDeFantasma/settings.py
@@ -9,9 +9,11 @@ https://docs.djangoproject.com/en/5.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv # Importa a função load_dotenv
 from pathlib import Path
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xflc97p69oq#5ccp4+drl2ca!d3ypd6d!k8upucea%9wm1z=dp'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
